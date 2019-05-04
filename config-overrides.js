b07816c357d17ad8a23dd7457d45557caf7fc81a
Chris Wei <yunzhi.wei@live.com>
2019-05-04 20:26:52 +0800

    feat (style/less): Adding a Less Stylesheet

--- config-overrides.js
+++ config-overrides.js
@@ -1,18 +1,19 @@
 const {
   override,
   fixBabelImports,
-  // addLessLoader,
+  addLessLoader,
 } = require("customize-cra");
  
 module.exports = override(fixBabelImports(
   "import", 
   {
     libraryName: "antd-mobile", 
-    // libraryDirectory: "es", 
-    style: 'css' // change importing css to less
-  },
-  // addLessLoader({
-  //   javascriptEnabled: true,
-  //   modifyVars: { "@primary-color": "#1DA57A" }
-  // })
-));
+    libraryDirectory: "es", 
+    style: true // change importing css to less
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: { '@primary-color': '#1DA57A' },
+    localIdentName: '[local]--[hash:base64:5]'
+  })
+);
